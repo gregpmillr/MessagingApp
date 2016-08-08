82fb78b1bc971fa4804106fa8df32f5623c04a03
greg <gregpmillr@gmail.com>
2016-08-08 13:16:14 -0300

    send message working

--- MessagingApp/app/Http/Controllers/Auth/AuthController.php
+++ MessagingApp/app/Http/Controllers/Auth/AuthController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Social_Profile;
+use Symfony\Component\EventDispatcher\Tests\Service;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Services_Twilio;
 
 class AuthController extends Controller
 {
@@ -23,6 +25,22 @@ class AuthController extends Controller
     */
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+
+public function message()
+{
+    $sid = "AC84452b0842ec4a4ccac82994298db5c9";
+    $token = "1c7b15119a35a2a1a7e74d2ba743ce37";
+
+    $client = new Services_Twilio($sid,$token);
+    $message = $client->account->messages->sendMessage(
+        '2044001758', // From a valid Twilio number
+        '9022105887', // Text this number
+        "Hello monkey!"
+    );
+
+    print $message->sid;
+}
+
 
     /**
      * Create a new authentication controller instance.
@@ -63,7 +81,7 @@ class AuthController extends Controller
     {
         //create user
         //create social profile associated with user
-
+        $this->message();
         $user = new User();
         $user->businessName = $data['businessName'];
         $user->preferredFName = $data['fname'];
